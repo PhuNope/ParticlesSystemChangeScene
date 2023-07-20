a88613947872dbd903644545af08207ea677f28d
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-07-20 15:32:12 +0700

    animation spider win

--- assets/scripts/controller2.ts
+++ assets/scripts/controller2.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, misc, Node, Quat, Sprite, SpriteFrame, Tween, tween, Vec2, Vec3 } from 'cc';
+import { _decorator, Component, DistanceJoint2D, EPhysics2DDrawFlags, Graphics, misc, Node, PhysicsSystem2D, Quat, RigidBody2D, Sprite, SpriteFrame, Tween, tween, Vec2, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('controller2')
@@ -7,20 +7,41 @@ export class controller2 extends Component {
     @property(Node)
     card: Node = null;
 
-    start() {
-        let that = this;
-        this.card._uiProps.uiTransformComp.setAnchorPoint(new Vec2(0.5, 1));
-        let x = 300;
-        tween({ x: 300, y: 0 })
-            .to(5, { x: 0, y: 600 }, {
-                onUpdate(target: { x: number, y: number; }, ratio) {
-                    let x = target.x * Math.sin(misc.degreesToRadians(target.y * 2.5));
-                    that.card.position = new Vec3(x, target.y, 0);
+    @property(Graphics)
+    graphics: Graphics = null;
 
-                    that.changeAngle(new Vec3(0, 600, 0), that.card);
-                },
-            })
-            .start();
+    private L = 10; // Chiều dài
+    private g = 9.8; // Trọng trường
+    private b = 2;   // Hệ số b
+    private m = 5;   // Hệ số m
+
+
+    start() {
+        this.scheduleOnce(() => {
+            let that = this;
+            this.card._uiProps.uiTransformComp.setAnchorPoint(new Vec2(0.5, 1));
+
+            let cycleStart = misc.radiansToDegrees(Math.asin(this.card.position.x / Math.abs(this.card.position.x))) / this.card.position.y;
+
+            tween({ width: Math.abs(this.card.position.x), deltaLength: this.card.position.y, cycle: cycleStart })
+                .to(7, { width: 0, deltaLength: 1000, cycle: 0.5 }, {
+                    easing: 'linear',
+                    onUpdate(target: { width: number, deltaLength: number, cycle: number; }, ratio) {
+                        console.log(misc.degreesToRadians(target.deltaLength * target.cycle) / Math.PI);
+
+                        let x = target.width * Math.sin(misc.degreesToRadians(target.deltaLength * target.cycle));
+                        that.card.position = new Vec3(x, target.deltaLength, 0);
+
+                        that.changeAngle(new Vec3(0, 1000, 0), that.card);
+
+                        that.graphics.clear();
+                        that.graphics.moveTo(0, 1000);
+                        that.graphics.lineTo(that.card.position.x, that.card.position.y);
+                        that.graphics.stroke();
+                    },
+                })
+                .start();
+        }, 1);
     }
 
     changeAngle(direct: Vec3, target: Node) {
